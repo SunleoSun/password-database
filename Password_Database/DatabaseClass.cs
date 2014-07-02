f02b32b2dd3a1411b3850ecfc8e1205358cdede6
SuleoSun <SunleoSun123@gmail.com>
2014-07-02 14:16:39 +0300

    init

---
+++ Password_Database/DatabaseClass.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Password_Database
+{
+    public class DatabaseClass
+    {
+        string sAllData;
+        string[] sAllDatabase;
+        int NumDimensions = 0;
+        public string CurrentCategory;
+        //===========================================
+        public DatabaseClass()
+        {
+            StreamReader sr = new StreamReader("Database.txt");
+            sAllData = sr.ReadToEnd();
+            sr.Close();
+            sAllDatabase = sAllData.Split(new char[] {'\n','\r'}, StringSplitOptions.RemoveEmptyEntries);
+            NumDimensions = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumDimensions++;
+            }
+        }
+        //============================================
+        public void ViewCategory(string NameCategory, ListView listview)
+        {
+            if (NameCategory == "")
+                listview.Items.Clear();
+            NumDimensions = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumDimensions++;
+            }
+            listview.Items.Clear();
+            int CurrentLine = -1;
+            foreach (string line in sAllDatabase)
+            {
+                CurrentLine++;
+                if (!line.StartsWith("`"))
+                {
+                    if (line == NameCategory)
+                    {
+                        CurrentLine++;
+                        while (NumDimensions > CurrentLine && sAllDatabase[CurrentLine].StartsWith("`"))
+                        {
+                            string[] EditedLine = sAllDatabase[CurrentLine].Split(new char[] { '`' });
+                            int Counter = -1;
+                            int NumDimensionsEditedLine = 0;
+                            foreach (string line1 in EditedLine)
+                            {
+                                NumDimensionsEditedLine++;
+                            }
+                            foreach (string line1 in EditedLine)
+                            {
+                                Counter++;
+                                if (Counter + 1 > NumDimensionsEditedLine-1)
+                                    break;
+                                EditedLine[Counter] = EditedLine[Counter + 1];
+                            }
+                            int FillStrings = 0;
+                            foreach (string line1 in EditedLine)
+                            {
+                                if (line1.Length != 0)
+                                    FillStrings++;
+                                else
+                                    break;
+                            }
+                            Array.Resize<string>(ref EditedLine, FillStrings);
+                            ListViewItem lvi = new ListViewItem(EditedLine);
+                            listview.Items.Add(lvi);
+                            CurrentLine++;
+                            if (CurrentLine >= NumDimensions)
+                            {
+                                CurrentLine = -1;
+                                break;
+                            }
+                            //DataGridViewSelectedRowCollection r = new DataGridViewSelectedRowCollection();
+                        }
+                    }
+                }
+            }
+            CurrentLine = -1;
+        }
+        //=======================================
+        public void AddData(string NameCategory, string Name, string Account, string Password, string Else)
+        {
+            if (NameCategory == "")
+                return;
+            if (Name == "")
+            {
+                MessageBox.Show("Name must not be empty!");
+                return;
+            }
+            if (Account == "")
+                Account = "*************";
+            if (Password == "")
+                Password = "*************";
+            if (Else == "")
+                Else = "*************";
+
+            int SentenceNumber=-1;
+
+            foreach (string line in sAllDatabase)
+            {
+                SentenceNumber++;
+                if (NameCategory == line)
+                {
+                    if (SentenceNumber + 1 == NumDimensions)
+                        break;
+                    string Tmp = sAllDatabase[SentenceNumber+1];
+                    while (Tmp.StartsWith("`"))
+                    {
+                        if (Tmp.StartsWith("`" + Name + "`"))
+                        {
+                            MessageBox.Show("This name is already exists!");
+                            return;
+                        }
+                        else
+                        {
+                            SentenceNumber++;
+                            if (SentenceNumber + 1 == NumDimensions)
+                                break;
+                            Tmp = sAllDatabase[SentenceNumber + 1];
+                        }
+                    }
+                }
+            }
+            int NumSentence = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumSentence++;
+                if (line == NameCategory)
+                {
+                    Array.Resize<string>(ref sAllDatabase, NumDimensions + 1);
+                    //Замена и смещение элементов базового массива на одно измерение
+                    for (int x = NumDimensions; x > NumSentence - 1; x--)
+                    {
+                        sAllDatabase[x] = sAllDatabase[x-1]; 
+                    }
+                    sAllDatabase[NumSentence] = "`" + Name + "`" + Account + "`" + Password + "`" + Else + "`";
+                }
+            }
+        }
+        //==============================================
+        public void DeleteData(string NameCategory, string Name)
+        {
+            if (NameCategory == null || Name == "")
+                return;
+            string Tmp;
+            int NumSentence = -1;
+            NumDimensions = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumDimensions++;
+            }
+            foreach (string line in sAllDatabase)
+            {
+                NumSentence++;
+                if (NameCategory == line)
+                {
+                    Tmp = sAllDatabase[NumSentence+1];
+                    bool b = true;
+                    while (b)
+                    {
+                        NumSentence++;
+                        if (Tmp.StartsWith("`" + Name))
+                        {
+                            b = false;
+                            if (NumSentence == NumDimensions - 1)
+                            {
+                                Array.Resize<string>(ref sAllDatabase, NumDimensions - 1);
+                                return;
+                            }
+                            for (int x = NumSentence; x < NumDimensions - 1; x++)
+                            {
+                                sAllDatabase[x] = sAllDatabase[x + 1];
+                            }
+                        }
+                        else
+                        {
+                            Tmp = sAllDatabase[NumSentence + 1];
+                            b = true;
+                        }
+                    }
+                }
+            }
+            Array.Resize<string>(ref sAllDatabase, NumDimensions - 1);
+            
+        }
+        //==============================================
+        public void EditData(string NameCategory, string Name, string NewName,string NewAccount,string NewPassword,string NewElse)
+        {
+            if (NameCategory == ""|| Name == ""|| NewName == "")
+                return;
+            if (NewAccount == "")
+                NewAccount = "*************";
+            if (NewPassword == "")
+                NewPassword = "*************";
+            if (NewElse == "")
+                NewElse = "*************";
+            NumDimensions = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumDimensions++;
+            }
+            int NumSentence=-1;
+            int SentenceNumber = -1;
+            int NumOfRepeatedSentence = 0;
+            foreach (string line in sAllDatabase)
+            {
+                SentenceNumber++;
+                if (NameCategory == line)
+                {
+                    if (SentenceNumber + 1 == NumDimensions)
+                        break;
+                    string Tmp = sAllDatabase[SentenceNumber + 1];
+                    while (Tmp.StartsWith("`"))
+                    {
+                        if (Tmp.StartsWith("`" + NewName + "`"))
+                        {
+                            NumOfRepeatedSentence = SentenceNumber + 1;
+                            break;
+                        }
+                        else
+                        {
+                            SentenceNumber++;
+                            if (SentenceNumber + 1 == NumDimensions)
+                                break;
+                            Tmp = sAllDatabase[SentenceNumber + 1];
+                        }
+                    }
+                }
+            }
+            foreach (string line in sAllDatabase)
+            {
+                NumSentence++;
+                if (line == NameCategory)
+                {
+                    while (sAllDatabase[NumSentence+1].StartsWith("`"))
+                    {
+                        if (sAllDatabase[NumSentence+1].StartsWith("`" + Name + "`"))
+                        {
+                            if (NumSentence + 1 == NumOfRepeatedSentence)
+                            {
+                                sAllDatabase[NumSentence + 1] = "`" + NewName + "`" + NewAccount + "`" + NewPassword + "`" + NewElse + "`";
+                                break;
+                            }
+                            else
+                            {
+                                MessageBox.Show("This name already exists!");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            NumSentence++;
+                            if (NumSentence >= NumDimensions-1)
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        public bool SaveData()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("Database.txt"))
+                {
+                    foreach (string line in sAllDatabase)
+                    {
+                        sw.WriteLine(line);
+                    }
+                    return true;
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+        //==============================================
+        public void CreateDataCategorie(string NameCategory)
+        {
+            foreach (string line in sAllDatabase)
+            {
+                if (line == NameCategory)
+                {
+                    MessageBox.Show("This category already exists!");
+                    return;
+                }
+            }
+            CurrentCategory = NameCategory;
+            if (NameCategory != null)
+            {
+                //Если есть уже такая категория,- то выход из ф.
+                foreach (string line in sAllDatabase)
+                {
+                    if (line == NameCategory)
+                    {
+                        MessageBox.Show("This category already exists!");
+                        return;
+                    }
+                }
+                NumDimensions = 0;
+                foreach (string line in sAllDatabase)
+                {
+                    NumDimensions++;
+                }
+                Array.Resize<string>(ref sAllDatabase, NumDimensions + 1);
+                sAllDatabase[NumDimensions] = NameCategory;
+                return;
+            }
+        }
+        //=======================================
+        public void DeleteDataCategory(string NameCategory)
+        {
+            NumDimensions = 0;
+            foreach (string line in sAllDatabase)
+            {
+                NumDimensions++;
+            }
+            if (NameCategory != null)
+            {
+                int NumberOfDeletingSentence = -1;
+                int NumberOfAllDeletingSentences = 1;
+                int Counter=0;
+                //Определение количества удаляемых строк и их номера
+                foreach (string line in sAllDatabase)
+                {
+                    NumberOfDeletingSentence++;
+                    if (line == NameCategory)
+                    {
+                        Counter = NumberOfDeletingSentence;
+                        if (Counter+1 < NumDimensions)
+                            while (sAllDatabase[Counter+1].StartsWith("`"))
+                            {
+                                NumberOfAllDeletingSentences++;
+                                Counter++;
+                                if (Counter+1 >= NumDimensions)
+                                    break;
+                            }
+                        break;
+                    }
+                }
+                //Удаление строк
+                Counter = 0;
+                for (int x = 0; x < NumDimensions; x++)
+                {
+                    //Если второй индекс меньше кол-ва измерений массива
+                    if((NumberOfDeletingSentence + NumberOfAllDeletingSentences + x) < NumDimensions)
+                        sAllDatabase[x+NumberOfDeletingSentence] = sAllDatabase[(NumberOfDeletingSentence + NumberOfAllDeletingSentences + x)];
+                    else
+                    {
+                        //Если второй индекс больше кол-ва измерений массива то затереть оставшиеся string
+                        for (int y = x + NumberOfDeletingSentence; y < NumDimensions; y++)
+                        {
+                            Counter++;
+                            sAllDatabase[y] = "";
+                        }
+                        break;
+                    }
+                }
+                //Изменить размер массива, удаляя все пустые строки
+                //if (Counter == 0)
+                //    Counter = 1;
+                Counter = NumDimensions - Counter;
+                Array.Resize<string>(ref sAllDatabase, Counter);
+            }
+        }
+
+        //=============================================
+        public string[] FindAllCategoriesInFile()
+        {
+            int NumberCategories =  0;
+            foreach (string line in sAllDatabase)
+            {
+                if (!line.StartsWith("`"))
+                {
+                    NumberCategories++;
+                }
+            }
+            string[] Categories = new string[NumberCategories];
+            int Counter = 0;
+            foreach (string line in sAllDatabase)
+            {
+                if (!line.StartsWith("`"))
+                {
+                    Categories[Counter] = line;
+                    Counter++;
+                }
+            }
+            return Categories;
+        }
+        //=========================================
+        public bool ErrText(string Text)
+        {
+            for (int x = 0; x < Text.Length; x++)
+                if (Text[x] == '`')
+                    return true;
+            return false;
+        }
+
+    }
+}
---
+++ Password_Database/Edit.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Password_Database
+{
+    partial class Edit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbElse = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbAccount = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 185);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Else:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 19);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Account:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Name:";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(316, 272);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 52);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 272);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 52);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbElse
+            // 
+            this.tbElse.Location = new System.Drawing.Point(40, 208);
+            this.tbElse.Margin = new System.Windows.Forms.Padding(4);
+            this.tbElse.Name = "tbElse";
+            this.tbElse.Size = new System.Drawing.Size(505, 27);
+            this.tbElse.TabIndex = 13;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(40, 148);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(505, 27);
+            this.tbPassword.TabIndex = 12;
+            // 
+            // tbAccount
+            // 
+            this.tbAccount.Location = new System.Drawing.Point(40, 89);
+            this.tbAccount.Margin = new System.Windows.Forms.Padding(4);
+            this.tbAccount.Name = "tbAccount";
+            this.tbAccount.Size = new System.Drawing.Size(505, 27);
+            this.tbAccount.TabIndex = 11;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(40, 32);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(505, 27);
+            this.tbName.TabIndex = 10;
+            // 
+            // Edit
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(582, 358);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbElse);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbAccount);
+            this.Controls.Add(this.tbName);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Edit";
+            this.Text = "Edit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbElse;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbAccount;
+        private System.Windows.Forms.TextBox tbName;
+
+    }
+}
